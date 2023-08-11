20cfb7863e0ac5520985c24699e66de92ac751bd
PauloBezmon1 <137209259+PauloBezmon1@users.noreply.github.com>
2023-08-11 16:32:06 -0300

    Projeto

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './App.css';
+import Header from './components/Header';
+import MainPage from './components/HomePage';
+import Footer from './components/Footer';
+
+function App(){
+  return(
+    <div className='App'>
+      <Header />
+      <MainPage />
+      <Footer />
+    </div>
+  )
+};
+
+export default App;
